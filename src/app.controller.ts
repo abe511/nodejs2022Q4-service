1676fc2947b3cf5c7e6b4f58bccb2b849184802a
Art <absolutus11@gmail.com>
2023-02-06 01:46:31 +0400

    feat: application setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
+
+// import { Req, Res } from "@nestjs/common";
+// import { Request, Response } from "express";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Redirect("/user", HttpStatus.TEMPORARY_REDIRECT)
   getHello(): string {
     return this.appService.getHello();
   }
+  
+
+  // getHelloRes(@Req() req: Request, @Res() res: Response): string {
+  //   res.status(503).end("hehe");
+  //   return this.appService.getHello();
+  // }
+
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './controllers/user/user.controller';
+import { ArtistController } from './controllers/artist/artist.controller';
+import { AlbumController } from './controllers/album/album.controller';
+import { TrackController } from './controllers/track/track.controller';
+import { FavoritesController } from './controllers/favorites/favorites.controller';
+import { UserService } from './services/user/user.service';
+import { ArtistService } from './services/artist/artist.service';
+import { AlbumService } from './services/album/album.service';
+import { TrackService } from './services/track/track.service';
+import { FavoritesService } from './services/favorites/favorites.service';
+
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController, ArtistController, AlbumController, TrackController, FavoritesController],
+  providers: [AppService, UserService, ArtistService, AlbumService, TrackService, FavoritesService],
 })
 export class AppModule {}
